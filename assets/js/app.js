9a315a942a106f100900bf7b30523842a0299a4f
viktoriazaharova <zaharova181114@gmail.com>
2020-09-30 02:43:37 +0300

    add page

--- assets/js/app.js
+++ assets/js/app.js
@@ -364,3 +364,44 @@ $('.up').click(function () {
 $('.btn-cabinet-menu').click(function () {
    $(this).parents('.cabinet-left-column').toggleClass('open').find('.cabinet-menu').slideToggle();
 });
+
+$('.tabs-mobile-dropdown').click(function () {
+    $('.tabs-mobile-dropdown').removeClass('active');
+   $(this).toggleClass('active').next('.tabs__content').toggleClass('active');
+});
+
+// модальные окна (несколько)
+$(document).ready(function () {
+    var overlay = $('.overlay');
+    var open_modal = $('.open_modal');
+    var close = $('.modal__close, .overlay');
+    var modal = $('.modal__div');
+
+    open_modal.click(function (event) {
+        event.preventDefault();
+        var div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.click(function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
